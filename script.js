efbd4ffdad07c880cb24409df60f5e95075d77dd
kartik132000 <sujeetkumarsen37@gmail.com>
2025-03-03 17:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+
 function getFormvalue() {
     //Write your code here
+	event.preventDefault(); 
+
+    let fname = document.forms["form1"]["fname"].value.trim();
+    let lname = document.forms["form1"]["lname"].value.trim();
+
+    if (fname === "" || lname === "") {
+        alert("Please enter both first and last name.");
+        return;
+    }
+
+    alert(fname + " " + lname);
+
 
 }
+
